36f25fe861a8c7ac78d7a56d9269e24fdea49416
coderick <dev.erick95@gmail.com>
2020-01-26 23:21:14 -0500

    Finish

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.typed', {
+  strings: [
+	'<i class="mascota">Gato</i>',
+	'<i class="mascota">Perro</i>',
+	'<i class="mascota">Conejo</i>',
+	'<i class="mascota">Pez</i>',
+  ],
+  typeSpeed: 100,
+  loop: true,
+});
